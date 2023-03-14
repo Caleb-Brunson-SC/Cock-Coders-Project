8809b90552c23f6fd58d71ce301b3d671ec94c4a
jordan-wood02 <jcw43@email.sc.edu>
2023-03-14 16:26:34 -0400

    Adding Admin Class

---
+++ src/Admin.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Admin {
+    private ArrayList<Teacher> teachers;
+
+    public void addTeacher(Teacher teacher) {
+
+    }
+
+    public void removeTeacher(Teacher teacher) {
+
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Teacher {
+    private ArrayList<Course> coursesCreated;
+
+    public void removeStudent(Student student) {
+
+    }
+
+    public Topic createTopic() {
+        return null;
+    }
+
+    public Quiz createQuiz() {
+        return null;
+    }
+
+    public void delteComment() {
+        
+    }
+}
