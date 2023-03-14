c8307b235856845503e13356d70e83a8c3a91e6c
jordan-wood02 <jcw43@email.sc.edu>
2023-03-14 14:16:20 -0400

    Adding Comment Class

---
+++ src/Comment.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Comment {
+    private User user;
+    private String text;
+    private ArrayList<Comment> comments;
+    
+    public Comment(User user, String text) {
+        
+    }
+}
