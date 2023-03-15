52a5f6e76e7c7be0be5f47665a2fbf856193c77c
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-15 14:48:00 -0400

    Added getters and setters for each attribute

--- src/User.java
+++ src/User.java
@@ -31,6 +31,64 @@ public abstract class User {
         this.password = password;
     }
 
+    //GETTERS AND SETTERS
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //OTHER METHODS
     public User login(String username, String password) {
         return null;
     }
