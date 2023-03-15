2ec5d2887922d8294b9432fad8cef238d4765af8
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-14 22:12:49 -0400

    Changed userName to username to reflect users.json

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -4,7 +4,7 @@ public abstract class DataConstants {
     protected static final String USER_TYPE = "type";
     protected static final String USER_FIRST_NAME = "firstName";
     protected static final String USER_LAST_NAME = "lastName";
-    protected static final String USER_USER_NAME = "userName";
+    protected static final String USER_USER_NAME = "username";
     protected static final String USER_EMAIL = "email";
     protected static final String USER_DATE_OF_BIRTH = "dateOfBirth";
     protected static final String USER_PASSWORD = "password";
