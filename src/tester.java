fdd92d573a8ca43d1fec30a4e903f1807f6be25f
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-15 14:48:47 -0400

    Updated tester to test DataWriter

--- src/tester.java
+++ src/tester.java
@@ -1,11 +1,25 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class tester {
     public static void main(String args[])  {  
         System.out.println("Testing DataLoader");
 
-        ArrayList<User> users = DataLoader.getUsers();
-        System.out.println(users.toString());
+        System.out.println("--------------------------------------------------------------------\n");
+        UserList us = UserList.getInstance();
+        printUsers(us.getUsers());
         
-    }  
+        System.out.println("--------------------------------------------------------------------\n");
+        LocalDate date = LocalDate.of(2020, 1, 8);
+        us.addUser("admin", "bob", "jones", "bj32", "bj@gmail.com", date, "5892759fshkag");
+        printUsers(us.getUsers());
+        
+        //us.saveUsers(); works but removed indentation formatting on users.json
+    }
+
+    public static void printUsers(ArrayList<User> users) {
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println("***" + users.get(i));
+        }
+    }
 }
