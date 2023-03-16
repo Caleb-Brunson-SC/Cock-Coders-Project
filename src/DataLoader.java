1ee54cb7910f55876239a98d9474ab56f830ffaa
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-16 09:43:31 -0400

    Format updates

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -13,15 +13,15 @@ public class DataLoader extends DataConstants {
         
         try {
             FileReader reader = new FileReader(USER_FILE_NAME);
-            JSONParser parser = new JSONParser();	
-			JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
+            JSONParser parser = new JSONParser();
+            JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
 
             for(int i=0; i < usersJSON.size(); i++) {
                 JSONObject userJSON = (JSONObject)usersJSON.get(i);
-				UUID id = UUID.fromString((String)userJSON.get(USER_ID));
+                UUID id = UUID.fromString((String)userJSON.get(USER_ID));
                 String type = (String)userJSON.get(USER_TYPE);
-				String firstName = (String)userJSON.get(USER_FIRST_NAME);
-				String lastName = (String)userJSON.get(USER_LAST_NAME);
+                String firstName = (String)userJSON.get(USER_FIRST_NAME);
+                String lastName = (String)userJSON.get(USER_LAST_NAME);
                 String userName = (String)userJSON.get(USER_USER_NAME);
                 String email = (String)userJSON.get(USER_EMAIL);
                 LocalDate dateOfBirth = LocalDate.parse((String)userJSON.get(USER_DATE_OF_BIRTH)); // must be ISO_LOCAL_DATE format of (YYYY-MM-DD)
