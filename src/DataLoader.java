84aa3ff8a4fba9a1202efcc7e59f08b6642c0cbf
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-17 17:17:28 -0400

    Updated data loading feature

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -47,7 +47,75 @@ public class DataLoader extends DataConstants {
 
     public static ArrayList<Course> getCourses() {
         ArrayList<Course> courses = new ArrayList<Course>();
+        
+        try {
+            // UserList to get Users by thier UUIDs
+            UserList users = UserList.getInstance();
 
+            // JSON FILE
+            FileReader reader = new FileReader(COURSE_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray coursesJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for (Object o : coursesJSON) {
+                // Instatiate objects
+                Teacher teacher;
+                ArrayList<Topic> topics = new ArrayList<Topic>();
+                ArrayList<Review> reviews = new ArrayList<Review>();
+                ArrayList<Comment> comments = new ArrayList<Comment>();
+                ArrayList<StudentProgress> studentProgresses = new ArrayList<StudentProgress>();
+
+                // JSON course object
+                JSONObject courseJSON = (JSONObject) o;
+                UUID id = UUID.fromString((String)courseJSON.get(USER_ID));
+                String title = (String)courseJSON.get(COURSE_TITLE);
+                Language language = Enum.valueOf(Language.class, (String)courseJSON.get(COURSE_LANGUAGE));
+                String description = (String)courseJSON.get(COURSE_DESCRIPTION);
+                UUID teacherID = UUID.fromString((String)courseJSON.get(COURSE_TEACHER_ID));
+                teacher = (Teacher) users.getUserByUUID(teacherID);
+
+                JSONArray studentProgressesJSON = (JSONArray)courseJSON.get(COURSE_STUDENTS);
+                for (Object s : studentProgressesJSON) {
+                    JSONObject studentProgressJSON = (JSONObject) s;
+                    UUID studentID = UUID.fromString((String)studentProgressJSON.get(COURSE_STUDENT_ID));
+                    Student student = (Student) users.getUserByUUID(studentID);
+                    double[] quizGrades = (double[])studentProgressJSON.get(COURSE_QUIZ_GRADES);
+                    int currentTopicIndex = (int)studentProgressJSON.get(COURSE_CURRENT_TOPIC_INDEX);
+
+                    studentProgresses.add(new StudentProgress(student, quizGrades, currentTopicIndex));
+                }
+
+                JSONArray topicsJSON = (JSONArray)courseJSON.get(COURSE_TOPICS);
+                for (Object t : topicsJSON) {
+
+                }
+
+                JSONArray commentsJSON = (JSONArray)courseJSON.get(COURSE_COMMENTS);
+                for (Object c : commentsJSON) {
+
+                }
+
+                JSONArray reviewsJSON = (JSONArray)courseJSON.get(COURSE_REVIEWS);
+                for (Object r : reviewsJSON) {
+                    JSONObject reviewJSON = (JSONObject) r;
+                    UUID reviewID = UUID.fromString((String)reviewJSON.get(COURSE_ID));
+                    UUID studentID = UUID.fromString((String)reviewJSON.get(COURSE_STUDENT_ID));
+                    Student student = (Student)users.getUserByUUID(studentID);
+                    LocalDate date = LocalDate.parse((String)reviewJSON.get(COURSE_DATE));
+                    int rating = (int)reviewJSON.get(COURSE_RATING);
+                    String content = (String)reviewJSON.get(COURSE_CONTENT);
+
+                    reviews.add(new Review(reviewID, student, date, rating, content));
+                }
+
+                courses.add(new Course(id, title, language, description, teacher, topics, reviews, comments, studentProgresses));
+            }
+
+            return courses;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
--- src/dataTester.java
+++ src/dataTester.java
@@ -6,21 +6,30 @@ public class dataTester {
         System.out.println("Testing DataLoader");
 
         System.out.println("--------------------------------------------------------------------\n");
-        UserList us = UserList.getInstance();
-        printUsers(us.getUsers());
+        //UserList us = UserList.getInstance();
+        //printUsers(us.getUsers());
+        CourseList cs = CourseList.getInstance();
+        printCourses(cs.getCourses());
         
+        /*
         System.out.println("--------------------------------------------------------------------\n");
         LocalDate date = LocalDate.of(2020, 1, 8);
         us.addUser("admin", "bob", "jones", "bj32", "bj@gmail.com", date, "5892759fshkag");
         printUsers(us.getUsers());
         
         //us.saveUsers();
-
+        */
     }
 
     public static void printUsers(ArrayList<User> users) {
         for (int i = 0; i < users.size(); i++) {
             System.out.println("***" + users.get(i));
+        }
+    }
+
+    public static void printCourses(ArrayList<Course> courses) {
+        for (int i = 0; i < courses.size(); i++) {
+            System.out.println("***" + courses.get(i));
         }
     }
 }
