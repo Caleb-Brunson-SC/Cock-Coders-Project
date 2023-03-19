2b18ad41623a58fa8d73b43b5f0d4a9fa435f35d
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-19 16:45:40 -0400

    Added features to saveCourses(), getCourseJSON(), and added a main method for testing

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -38,7 +38,38 @@ public class DataWriter extends DataConstants {
     }
 
     public static void saveCourses() {
+        CourseList courses = CourseList.getInstance();
+        ArrayList<Course> courseList = courses.getCourses();
+        JSONArray jsonCourses = new JSONArray();
 
+        // Creation of Objects in JSON 
+		for(int i=0; i< courseList.size(); i++) {
+			jsonCourses.add(getCourseJSON(courseList.get(i)));
+		}
+
+        // Write the JSON file
+        try (FileWriter file = new FileWriter("json/test.json")) { // Change this to COURSE_FILE_NAME
+            file.write(jsonCourses.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getCourseJSON(Course course) {
+        JSONObject courseDetails = new JSONObject();
+        courseDetails.put(COURSE_ID, course.getId().toString());
+        courseDetails.put(COURSE_TITLE, course.getTitle());
+        courseDetails.put(COURSE_LANGUAGE, course.getLanguage().toString());
+        courseDetails.put(COURSE_DESCRIPTION, course.getDescription());
+        courseDetails.put(COURSE_TEACHER_ID, course.getTeacher().getId().toString());
+        return courseDetails;
+    }
+
+    public static void main(String args[])  {  
+        //Test
+        CourseList cs = CourseList.getInstance();
+        cs.saveCourses();
     }
 
 }
