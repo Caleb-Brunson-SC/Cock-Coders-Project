b806d1e790733e02c3f48296806f164fbbde0066
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-03-21 12:31:37 -0400

    Removed attributes to Admin, Student, Teacher classes

--- src/backEnd/Student.java
+++ src/backEnd/Student.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Student extends User {
-    private StudentProgress student;
-    private ArrayList<StudentProgress> coursesEnrolled;
-    private ArrayList<StudentProgress> coursesCompleted;
 
     public Student(UUID id, String firstName, String lastName, String userName, String email, LocalDate dateOfBirth, String password) {
         super(id, firstName, lastName, userName, email, dateOfBirth, password);
@@ -16,7 +13,4 @@ public class Student extends User {
         super(firstName, lastName, userName, email, dateOfBirth, password);
     }
 
-    public String getName() {
-        return getFirstName() + " " + getLastName();
-    }
 }
