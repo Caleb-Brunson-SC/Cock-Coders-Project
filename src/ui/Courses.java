95e143f1c6ea7e8982ccad229f4a7aa761bb1a53
Nicholas Reiss <njreiss33@gmail.com>
2023-03-27 14:19:28 -0400

    I dont really know if i've made any changes

--- src/ui/Courses.java
+++ src/ui/Courses.java
@@ -20,6 +20,7 @@ public class Courses implements ActionListener{
     frame1 = new JFrame();
     tabbedPane = new JTabbedPane();
 
+    frame1.setLayout(null);
     // for statement should populate string J with the title of the course for
     // each course the student is enrolled in. Should iterate once for each 
     // course in courses array. 
@@ -27,41 +28,39 @@ public class Courses implements ActionListener{
     // for (int i = 0; i < courses.size(); i++) {
       // Course workingCourse = courses.get(i);
       JPanel p1 = new JPanel();
+      p1.setLayout(null);
       String j = i + " ";
       tabbedPane.add(j, p1);
       JLabel courseName = new JLabel();
       JLabel authorName = new JLabel();
       JLabel courseDescription = new JLabel();
-      JLabel lessonLabel = new JLabel();
-      JLabel quizLabel = new JLabel();
+      JLabel topicLabel = new JLabel();
 
       courseName.setText("Course Name");
       authorName.setText("Author Name");
       courseDescription.setText("Course Description");
-      lessonLabel.setText("Lessons: ");
-      quizLabel.setText("Quiz: ");
+      topicLabel.setText("Topics: ");
 
       courseName.setFont(new Font(courseName.getFont().getName(), Font.BOLD, courseName.getFont().getSize()));
+      topicLabel.setFont(new Font(courseName.getFont().getName(), Font.BOLD, courseName.getFont().getSize()));
 
-      courseName.setBounds(100, 100, 300, 20);
-      authorName.setBounds(100, 140, 300, 20);
-      courseDescription.setBounds(100, 180, 300, 20);
-      lessonLabel.setBounds(100, 220, 300, 20);
-      quizLabel.setBounds(100, 260, 300, 20);
+      courseName.setBounds(50, 50, 300, 20);
+      authorName.setBounds(50, 70, 300, 20);
+      courseDescription.setBounds(50, 100, 300, 20);
+      topicLabel.setBounds(50, 130, 300, 20);
 
       p1.add(courseName);
       p1.add(authorName);
       p1.add(courseDescription);
-      p1.add(lessonLabel);
-      p1.add(quizLabel);
+      p1.add(topicLabel);
       // tabbedPane.add(workingCourse.getTitle() , p1);
+
 
     }
     tabbedPane.setBounds(0,0,500,500);
     
     frame1.setVisible(true);
     frame1.setSize(500, 500);
-    frame1.setLayout(null);
 
     frame1.add(tabbedPane);
 
