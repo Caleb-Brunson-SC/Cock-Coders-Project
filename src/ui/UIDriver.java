b431de2d3e30891e6f257a3495eb960fab0538d6
jordan-wood02 <jcw43@email.sc.edu>
2023-03-28 15:22:12 -0400

    Fixing ViewQuestion

--- src/ui/UIDriver.java
+++ src/ui/UIDriver.java
@@ -4,12 +4,7 @@ import javax.swing.*;
 
 public class UIDriver {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         new ViewQuestion(null);
-
-=======
-        new ViewCourses(null);
->>>>>>> d004b659c14705b044c7a4c4e2fc726a993ca04a
     }
 }
--- src/ui/ViewQuestion.java
+++ src/ui/ViewQuestion.java
@@ -9,18 +9,27 @@ public class ViewQuestion implements ActionListener {
     private final LMSFacade facade;
     JFrame frame1;
     JLabel l1;
+    JRadioButton r1;
+    JRadioButton r2;
+    JRadioButton r3;
+    JRadioButton r4;
+
 
     ViewQuestion(LMSFacade facade) {
         this.facade = facade;
         frame1 = new JFrame();
-        JRadioButton r1 = new JRadioButton("A) ");
-        JRadioButton r2 = new JRadioButton("B) ");
-        JRadioButton r3 = new JRadioButton("C) ");
-        JRadioButton r4 = new JRadioButton("D) ");
-        r1.setBounds(75,100,100,30);    
-        r2.setBounds(75,100,100,30);
-        r3.setBounds(75,100,100,30);
-        r4.setBounds(75,100,100,30);
+        l1 = new JLabel();
+        l1.setText("Question: ");
+        l1.setBounds(100,50,300,30);
+
+        r1 = new JRadioButton("A) ");
+        r2 = new JRadioButton("B) ");
+        r3 = new JRadioButton("C) ");
+        r4 = new JRadioButton("D) ");
+        r1.setBounds(100,100,100,30);    
+        r2.setBounds(100,130,100,30);
+        r3.setBounds(100,160,100,30);
+        r4.setBounds(100,190,100,30);
 
         ButtonGroup bg = new ButtonGroup();
         bg.add(r1);
@@ -28,12 +37,13 @@ public class ViewQuestion implements ActionListener {
         bg.add(r3);
         bg.add(r4);
 
+        frame1.add(l1);
         frame1.add(r1);
         frame1.add(r2);
         frame1.add(r3);
         frame1.add(r4);
 
-        frame1.setSize(300,300);
+        frame1.setSize(500,500);
         frame1.setLayout(null);    
         frame1.setVisible(true);  
     } 
