88fda092bf755d87328830f8c3da600d3d3114f8
Nicholas Reiss <njreiss33@gmail.com>
2023-04-01 16:28:56 -0400

    Fixed viewDashboard

--- src/ui/viewDashboard.java
+++ src/ui/viewDashboard.java
@@ -1,71 +1,97 @@
 package ui;
+import java.util.ArrayList;
+import backEnd.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-import backEnd.*;
+import java.awt.event.*;
 
-public class viewDashboard {
-    private final LMSFacade facade;
-    private final Course course;
-    private final Grade grade;
-    private JFrame frame;
+public class viewDashboard implements ActionListener{
+  private final LMSFacade facade;
+  JFrame frame1;
+  ArrayList<Course> courses;
+  JLabel coursesLabel;
+  User user;
 
-    viewDashboard(LMSFacade facade) {
-        this.facade = facade;
-        course = new Course();
-        frame = new JFrame();
-        grade = new Grade();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
-        frame.setLocationRelativeTo(null);
-        
-        // Welcome User
-        JLabel welcomeLabel = new JLabel("Welcome, " + facade.getUser().getUserName() + "!");
-        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
-        frame.getContentPane().add(welcomeLabel, BorderLayout.NORTH);
+  viewDashboard(LMSFacade facade){
+    this.facade = facade;
+    this.courses = facade.getCourseList().getCourses();  
+    this.user = facade.getUser();  
+    
+    frame1 = new JFrame();
 
-        // Show User Email
-        JPanel infoPanel = new JPanel(new GridLayout(2, 1));
-        JLabel emailLabel = new JLabel("Email: " + facade.getUser().getEmail());
-        infoPanel.add(emailLabel);
+    frame1.setLayout(null);
+    // for statement should populate string J with the title of the course for
+    // each course the student is enrolled in. Should iterate once for each 
+    // course in courses array. 
+    coursesLabel = new JLabel();
+    coursesLabel.setText("Completed Courses");
+    coursesLabel.setFont(new Font(coursesLabel.getFont().getName(), Font.BOLD, coursesLabel.getFont().getSize()));
 
-        // Show Course Progress
-        ArrayList<Grade> grades = StudentProgress.getGrades();
-        double totalGrade = 0.0;
-        if(grades != null) {
-            for (Grade grade : grades) {
-                totalGrade += Grade.getGradePercentage();
-            }
-        }
-        double avgGrade;
-        if(grades != null) {
-            avgGrade = totalGrade; /// grades.size();
-        } else {
-            avgGrade = totalGrade;
-        }
-        
-        JLabel gradeLabel = new JLabel(String.format("Course Progress: %.2f%%", avgGrade));
-        infoPanel.add(gradeLabel);
+    coursesLabel.setBounds(50, 50, 200, 20);
+    
 
-        // Certificate Button
-        JButton generateCertButton = new JButton("Generate Certificate");
-        generateCertButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame certFrame = new JFrame();
-                certFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                certFrame.setSize(400, 300);
-                certFrame.setLocationRelativeTo(null);
-                JLabel certLabel = new JLabel("Congratulations, " + facade.getUser().getUserName() + "! You have completed the course.");
-                certLabel.setHorizontalAlignment(JLabel.CENTER);
-                certFrame.getContentPane().add(certLabel);
-                certFrame.setVisible(true);
-            }
-        });
-        frame.getContentPane().add(infoPanel, BorderLayout.CENTER);
-        frame.setVisible(true);
+    frame1.add(coursesLabel);
+
+
+
+    for (int i = 0; i < courses.size(); i++) {
+    // for (int i = 0; i < courses.size(); i++) {
+      Course workingCourse = courses.get(i);
+      frame1.setLayout(null);
+      
+      String j = i + " ";
+      JLabel courseName = new JLabel();
+      JButton viewButton = new JButton();
+      JButton printButton = new JButton();
+      // JLabel 
+
+    
+      courseName.setText(workingCourse.getTitle()); // Course name
+      viewButton.setText("view");
+      printButton.setText("print");
+
+      courseName.setFont(new Font(courseName.getFont().getName(), Font.BOLD, courseName.getFont().getSize()));
+
+      courseName.setBounds(50, 80 + (i * 30), 150, 20);
+      viewButton.setBounds(200, 80 + (i * 30), 100, 20);
+      printButton.setBounds(300, 80 + (i * 30), 100, 20);
+     
+
+      frame1.add(courseName);
+      frame1.add(viewButton);
+      frame1.add(printButton);
+      // should parse through all topics in course
     }
+    
+
+    
+    frame1.setVisible(true);
+    frame1.setSize(600, 600);
+
+  }
+  public void actionPerformed(ActionEvent e) {
+    JButton btn = (JButton)e.getSource();
+    String[] splitArray = btn.getName().split("\\s+");
+    String action = splitArray[0];
+    int courseBtnIndex = Integer.parseInt(splitArray[1]);
+    int topicBtnIndex = Integer.parseInt(splitArray[2]);
+    Course courseOfInterest = courses.get(courseBtnIndex);
+    Topic topicOfInterest = courseOfInterest.getTopics().get(topicBtnIndex);
+    if (action.equals("edit")) {
+      // Edit the topic information
+      new AddTopic(facade, topicOfInterest, true);
+      frame1.setVisible(false);
+    } else if (action.equals("delete")) {
+      // Delete the topic
+      facade.deleteTopic(courseOfInterest, topicOfInterest);
+      frame1.setVisible(false);
+    } else if (action.equals("view")) {
+      // View that particular topic
+      new ViewTopic(facade, courseOfInterest, topicOfInterest);
+    } else if (action.equals("viewcomments")) {
+      new ViewComments(facade, courseOfInterest.getComments());
+    } else if (action.equals("addcomment")) {
+      new AddComment(facade, courseOfInterest.getComments());
+    }
+  }
 }
