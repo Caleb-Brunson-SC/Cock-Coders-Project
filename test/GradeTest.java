f1d47dd8a220eff3ca1d62e8b9d6836b190c827d
jordan-wood02 <jcw43@email.sc.edu>
2023-04-09 23:00:17 -0400

    GradeTest

--- test/GradeTest.java
+++ test/GradeTest.java
@@ -3,38 +3,58 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import java.util.UUID;
+
+import org.junit.Before;
 import org.junit.Test;
 import backEnd.Grade;
 
 public class GradeTest {
-
+    private UUID id;
+    private UUID quizId;
+    @Before
+    public void setUp() {
+        String uuidString = "00000000-0000-0000-0000-000000000000";
+        id = UUID.fromString(uuidString);
+        quizId = UUID.fromString(uuidString);
+    }
     
     @Test
-    void testGradeDefaultConstructor() {
+    public void testGradeDefaultConstructor() {
         Grade grade = new Grade();
-        assertEquals(grade.getId(), Grade.NIL_UUID);
-        assertEquals(grade.getQuizID(), Grade.NIL_UUID);
-        assertEquals(Grade.getGradePercentage(), -1);
+        assertEquals(Grade.NIL_UUID, grade.getId());
+        assertEquals(Grade.NIL_UUID, grade.getQuizID());
+        assertEquals(-1.0, grade.getGradePercentage(), 0.001);
     }
 
     @Test
-    void testGradeConstructorWithId() {
-        UUID id = UUID.randomUUID();
-        UUID quizID = UUID.randomUUID();
-        double gradePercentage = 80.5;
+    public void testGradeConstructorWithId() {
+        String uuidString = "00000000-0000-0000-0000-000000000000";
+        UUID id = UUID.fromString(uuidString);
+        UUID quizID = UUID.fromString(uuidString);
+        double gradePercentage = 80.0;
         Grade grade = new Grade(id, quizID, gradePercentage);
-        assertEquals(grade.getId(), id);
-        assertEquals(grade.getQuizID(), quizID);
-        assertEquals(Grade.getGradePercentage(), gradePercentage);
+        assertEquals(id, grade.getId());
+        assertEquals(quizID, grade.getQuizID());
+        assertEquals(gradePercentage, Grade.getGradePercentage(), 0.001);
     }
 
     @Test
-    void testGradeConstructorWithoutId() {
-        UUID quizID = UUID.randomUUID();
-        double gradePercentage = 70.2;
-        Grade grade = new Grade(quizID, gradePercentage);
-        assertNotNull(grade.getId());
-        assertEquals(grade.getQuizID(), quizID);
-        assertEquals(grade.getGradePercentage(), gradePercentage);
+    public void testGradeConstructorWithoutId() {
+        UUID quizId = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        double gradePercentage = 85.0;
+        Grade grade = new Grade(quizId, gradePercentage);
+        assertEquals(Grade.NIL_UUID, grade.getId());
+        assertEquals(quizId, grade.getQuizID());
+        assertEquals(gradePercentage, Grade.getGradePercentage(), 0.001);
     }
+
+    @Test
+    public void testToString() {
+        UUID id = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        UUID quizID = UUID.fromString("22222222-2222-2222-2222-222222222222");
+        double gradePercentage = 87.5;
+        Grade grade = new Grade(id, quizID, gradePercentage);
+        String expectedOutput = "Grade [id=00000000-0000-0000-0000-000000000000, quizID=22222222-2222-2222-2222-222222222222, gradePercentage=87.5]";
+        assertEquals(expectedOutput, grade.toString());
+}
 }
