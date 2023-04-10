82062e626ce3bfc90d3fd609e10b598f2836e4e6
jordan-wood02 <jcw43@email.sc.edu>
2023-04-09 22:44:01 -0400

    GradeTest update

--- test/GradeTest.java
+++ test/GradeTest.java
@@ -1,5 +1,40 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.util.UUID;
+import org.junit.Test;
+import backEnd.Grade;
+
 public class GradeTest {
+
     
+    @Test
+    void testGradeDefaultConstructor() {
+        Grade grade = new Grade();
+        assertEquals(grade.getId(), Grade.NIL_UUID);
+        assertEquals(grade.getQuizID(), Grade.NIL_UUID);
+        assertEquals(Grade.getGradePercentage(), -1);
+    }
+
+    @Test
+    void testGradeConstructorWithId() {
+        UUID id = UUID.randomUUID();
+        UUID quizID = UUID.randomUUID();
+        double gradePercentage = 80.5;
+        Grade grade = new Grade(id, quizID, gradePercentage);
+        assertEquals(grade.getId(), id);
+        assertEquals(grade.getQuizID(), quizID);
+        assertEquals(Grade.getGradePercentage(), gradePercentage);
+    }
+
+    @Test
+    void testGradeConstructorWithoutId() {
+        UUID quizID = UUID.randomUUID();
+        double gradePercentage = 70.2;
+        Grade grade = new Grade(quizID, gradePercentage);
+        assertNotNull(grade.getId());
+        assertEquals(grade.getQuizID(), quizID);
+        assertEquals(grade.getGradePercentage(), gradePercentage);
+    }
 }
